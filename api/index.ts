cca802198e81527aff2355f512da0972efaa1378
Mahfoud <binsabbah2013@gmail.com>
2025-02-09 11:43:47 +0300

    init

---
+++ api/index.ts
@@ -0,0 +1,8 @@
+const express=require("express");
+const app =express();
+
+app.get('/',(req,res)=>res.send("Express on Vercel"));
+
+app.listen(3000,()=>console.log("server ready on port 3000"))
+
+module.export=app;
