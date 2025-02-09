bd39d967e3e3beb95f93b13faddada6b05d888bb
Mahfoud <binsabbah2013@gmail.com>
2025-02-09 12:06:17 +0300

    config port

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 const  express =require("express");
+require('dotenv').config()
+
 const app =express();
+
 
 app.get('/',(req,res)=>res.send("Express on Vercel"));
 
-app.listen(3000,()=>console.log("server ready on port 3000"))
+app.listen(process.env.PORT,()=>console.log("server ready on port ",process.env.PORT))
 
 module.export=app;
 //nothing
